47af1a1415c11aca5cfc5db25f3bd5aa5963797a
anuarharb <anuarharb@hotmail.com>
2017-07-20 14:37:39 -0500

    modal window

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log('hola');
+(function() {
+  'use strict'
+
+  document.getElementById("button").addEventListener("click", activateModal);
+  document.getElementById("modal").addEventListener("click", deactivateModal);
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+        deactivateModal();
+    }
+});
+
+  function activateModal() {
+    var image = this.value;
+    var modal = document.getElementById('modal');
+    var modalShadow = document.getElementById('modal-shadow');
+    imageSelected(image);
+    modal.setAttribute('class', 'active');
+    modalShadow.setAttribute('class', 'active');
+  }
+
+  function deactivateModal() {
+    console.log(this);
+    var modal = document.getElementById('modal');
+    var modalShadow = document.getElementById('modal-shadow');
+    modal.setAttribute('class', '');
+    modalShadow.setAttribute('class', '');
+  }
+})();
